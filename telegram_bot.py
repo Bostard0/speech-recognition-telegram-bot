7c5b1ab6783a8eb725a969cc546443951482ece1
Nikita <batruk84@gmail.com>
2023-07-14 01:23:35 +0300

    bot almost downloading ogg voice file

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,21 +1,40 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ContextTypes, CommandHandler, MessageHandler, filters, ApplicationBuilder
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+TOKEN = '5905018015:AAGar6s-VtRlGh0GJKXfHg4Eo7zeUeyF8JI'
+BOT_USERNAME = '@audio_recognizerr_bot'
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Send me a telegram voice message and I will send you a text version of it")
+
+
+async def handle_voice(update, context):
+    voice = update.message.voice
+    file = await context.bot.get_file(voice.file_id)
+    file.download('audio-to-recognize.ogg')
+    update.message.reply_text('Downloading your audio')
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('5905018015:AAGar6s-VtRlGh0GJKXfHg4Eo7zeUeyF8JI').build()
+    print('Starting bot...')
+    app = ApplicationBuilder().token(TOKEN).build()
 
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
 
-    application.run_polling()
+    # Messages
+    app.add_handler(MessageHandler(filters.VOICE, handle_voice))
+
+    # Errors
+    app.add_error_handler(error)
+
+    # Polling huyoling
+    print('Polling...')
+    app.run_polling(poll_interval=3)
